7aa23e8faa5e2a6ac33622857bc5e69a1106990c
Sagar <sagar20032610@gmail.com>
2025-02-22 02:12:32 +0530

    url update

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://mentormate-vlyu.onrender.com/",
     credentials:true
 }));
  
